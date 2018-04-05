652bd0f0a70a4a42ca99cb8d2004aa49681e2869
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-04-05 08:44:09 +0200

    Node Htttp

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+    if (request.method === 'GET' && request.url === '/') {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        })
+    } else {
+        response.setHeader('Content-Type', 'image/jpeg')
+        response.statusCode = 404;
+        fs.readFile('./pinguin404.jpg', function(err, data) {
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        })
+    }
+});
+server.listen(8080);
